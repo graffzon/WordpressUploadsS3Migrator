8e72cd28e816f99d7bbeea2652ec768e225da809
Kirill Zonov <kzonov@babbel.com>
2018-10-06 20:27:21 +0200

    Added basic s3 upload

--- main.go
+++ main.go
@@ -1,40 +1,81 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func main() {
-  // wordpressDumpPath := os.Args[1]
-  // wordpressDumpBytes, err := ioutil.ReadFile(wordpressDumpPath)
+	// wordpressDumpPath := os.Args[1]
+	// wordpressDumpBytes, err := ioutil.ReadFile(wordpressDumpPath)
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
-  // wordpressDump := string(wordpressDumpBytes)
-  // fmt.Println(wordpressDump(1)) // remove later
-  //
+	// wordpressDump := string(wordpressDumpBytes)
+	// fmt.Println(wordpressDump(1)) // remove later
+	//
 
-  wordpressUploadsFolder := os.Args[2]
-  names := uploadsNames(wordpressUploadsFolder)
-  fmt.Println(len(names))
+	// Step 1. Create a list of files
+	wordpressUploadsFolder := os.Args[2]
+	files := uploadsNames(wordpressUploadsFolder)
+	fmt.Println(len(files))
+
+	// Step 2. Instead of creating a list, upload each file into S3
+	//  If smth is not uploaded - print the error.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	uploader := s3manager.NewUploader(sess)
+	fmt.Println(uploader)
+
+	bucket := "zonovme-assets"
+	key := "foo/bar" + files[0]
+	file, err := os.Open(files[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	upParams := &s3manager.UploadInput{
+		Bucket: &bucket,
+		Key:    &key,
+		Body:   file,
+	}
+
+	// Perform an upload.
+	result, err := uploader.Upload(upParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+
+	// Step 3. After file uploaded, find it in the dump and change the path
 }
 
 func uploadsNames(folder string) []string {
-  var names []string
+	// var files []os.FileInfo
+	var names []string
 
-  files, err := ioutil.ReadDir(folder)
+	files, err := ioutil.ReadDir(folder)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	for _, file := range files {
-    if file.IsDir() {
-      names = append(names, uploadsNames(folder + "/" + file.Name())...)
-    } else {
-      names = append(names, file.Name())
-    }
+		if file.IsDir() {
+			names = append(names, uploadsNames(folder+"/"+file.Name())...)
+		} else {
+			names = append(names, folder+file.Name())
+		}
 	}
-  return names
+	return names
 }
