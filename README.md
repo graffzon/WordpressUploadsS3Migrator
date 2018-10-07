7219a79cf81ab54ad647597138667bab0ec9809e
Kirill Zonov <kzonov@babbel.com>
2018-10-07 17:51:02 +0200

    Added compiled binary and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Migrator of Wordpress Uploads to S3
+Once I had a necessity to migrate my Wordpress blog from a plain hosting to the AWS. And one of the first steps I had to do is to migrate my uploads. I wanted to be safe and first upload them to the S3, then change the links in the hosted blog, and only then, after making sure it works, migrate the blog itself.
+Back then I haven't find any tool to help me with this, so I created this small script. Feel free to contribute.
+
+## Steps to migrate your uploads
+1. You have to get your SQL dump. Usually you can get it from the management panel of your hosting (in my case it was ISP Manager). Also find out in advance, how can upload the SQL dump back again when you modify it.
+2. As the next step, you need to download an archive with your files from hosting. There are a handful of those tools. I used BackupGuard for this, you can use whichever you want.
+3. Setup your S3 bucket in the AWS. You can do it either manually, or using the Terraform code, which you can find in the `infrastructure/` directory.
+4. Clone this repo and run the script with the following params:
+
+```bin/migrator /Users/username/Downloads/u8526307_default.sql /Users/username/Downloads/sg_backup_opt_20181006084001/wp-content/uploads http://zonov.me/wp-content/uploads bucket-for-assets```
+
+Where the first parameter is your SQL dump file, the second one is your downloaded uploads directory, the third param is your current website `uploads` folder, the fourth one is the name of your bucket in the S3.
+
+Here I'm assuming that your AWS S3 region is `eu-central-1`, if it isn't - feel free to change it in the code and then just run it with `go run`. (Before that you will need to install `go` and `dep`, f.e. using Homebrew).
+
+## Contribution
+I would really appreciate your contribution. Here are some things you could do:
+1. Improve logging
+2. Parallelise the files upload
+3. Make the script even more generic, probably extract the region into a param, if so - make it optional, etc.
+4. Add tests
+5. Improve the documentation
+
+If I haven't mention something, but you still want to contribute - feel free to submit an issue or a PR.
--- main.go
+++ main.go
@@ -12,32 +12,34 @@ import (
 )
 
 const bucketRegion = "eu-central-1"
-const bucketName = "zonovme-assets"
 
 func main() {
-	// Step 1. Create a list of files
+	// Reading command line params
+	wordpressDumpPath := os.Args[1]
 	wordpressUploadsFolder := os.Args[2]
+	wordpressUploadsWebPath := os.Args[3]
+	bucketName := os.Args[4]
+
+	// Step 1. Create a list of files
 	files := getUploadsNames(wordpressUploadsFolder)
-	fmt.Println(len(files))
+	fmt.Println("Number of files to be sent: " + string(len(files)))
 
 	// Step 2. Upload each file from the list into S3.
 	//  If smth is not uploaded - print the error.
-	err := uploadFilesToS3(files, wordpressUploadsFolder)
+	err := uploadFilesToS3(files, wordpressUploadsFolder, bucketName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Step 3. After file uploaded, find it in the dump and change the path
-	wordpressDumpPath := os.Args[1]
+	// Step 3. After files uploaded, find them in the dump and change the path
 	wordpressDumpBytes, err := ioutil.ReadFile(wordpressDumpPath)
 	if err != nil {
 		fmt.Println(err)
 	}
 	wordpressDump := string(wordpressDumpBytes)
 
-	wordpressUploadsWebPath := os.Args[3]
-	replaceOldURLsWithNew(files, wordpressDump, wordpressDumpPath, wordpressUploadsFolder, wordpressUploadsWebPath)
+	replaceOldURLsWithNew(files, wordpressDump, wordpressDumpPath, wordpressUploadsFolder, wordpressUploadsWebPath, bucketName)
 }
 
 func getUploadsNames(folder string) []string {
@@ -59,7 +61,7 @@ func getUploadsNames(folder string) []string {
 	return names
 }
 
-func uploadFilesToS3(files []string, wordpressUploadsFolder string) error {
+func uploadFilesToS3(files []string, wordpressUploadsFolder string, bucketName string) error {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(bucketRegion)},
 	)
@@ -68,14 +70,9 @@ func uploadFilesToS3(files []string, wordpressUploadsFolder string) error {
 		return err
 	}
 
-	uploader := s3manager.NewUploader(sess)
-
-	bucket := bucketName
 	acl := "public-read"
-
-	// Will be taken from the first upload
+	uploader := s3manager.NewUploader(sess)
 	objects := []s3manager.BatchUploadObject{}
-	// S3BasePath := ""
 
 	for _, filename := range files {
 		key := "uploads/" + strings.Replace(filename, wordpressUploadsFolder, "", 1)
@@ -86,7 +83,7 @@ func uploadFilesToS3(files []string, wordpressUploadsFolder string) error {
 		upParams := []s3manager.BatchUploadObject{
 			{
 				Object: &s3manager.UploadInput{
-					Bucket: &bucket,
+					Bucket: &bucketName,
 					Key:    &key,
 					Body:   file,
 					ACL:    &acl,
@@ -95,18 +92,7 @@ func uploadFilesToS3(files []string, wordpressUploadsFolder string) error {
 		}
 
 		objects = append(objects, upParams...)
-
-		// result, err := uploader.Upload(upParams)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		// if S3BasePath == "" {
-		// 	fmt.Println("key: " + strings.Replace(filename, wordpressUploadsFolder, "", 1))
-		// 	fmt.Println("location: " + result.Location)
-		// 	S3BasePath = strings.Replace(result.Location, strings.Replace(filename, wordpressUploadsFolder, "", 1), "", 1)
-		// }
 	}
-	// fmt.Println(S3BasePath)
 
 	iter := &s3manager.UploadObjectsIterator{Objects: objects}
 	if err := uploader.UploadWithIterator(aws.BackgroundContext(), iter); err != nil {
@@ -115,15 +101,16 @@ func uploadFilesToS3(files []string, wordpressUploadsFolder string) error {
 	return nil
 }
 
-func replaceOldURLsWithNew(files []string, wordpressDump string, wordpressDumpPath string, wordpressUploadsFolder string, wordpressUploadsWebPath string) {
-	// replace `uploads` with a variable
+func replaceOldURLsWithNew(files []string, wordpressDump string, wordpressDumpPath string, wordpressUploadsFolder string, wordpressUploadsWebPath string, bucketName string) {
+	// TODO: replace `uploads` with a variable
 	bucketUrl := "https://" + bucketName + ".s3." + bucketRegion + ".amazonaws.com/uploads"
 	for _, filename := range files {
 		fullFileNameInDump := strings.Replace(filename, wordpressUploadsFolder, wordpressUploadsWebPath, 1)
 		fullFileNameInS3 := strings.Replace(filename, wordpressUploadsFolder, bucketUrl, 1)
-		fmt.Println("fullFileNameInDump: " + fullFileNameInDump + "        \n fullFileNameInS3:" + fullFileNameInS3)
+		// Uncomment if you want to see all the files' names
+		// fmt.Println("fullFileNameInDump: " + fullFileNameInDump + "\n fullFileNameInS3:" + fullFileNameInS3)
 
-		// wordpressDump = strings.Replace(wordpressDump, fullFileNameInDump, fullFileNameInS3, -1)
+		wordpressDump = strings.Replace(wordpressDump, fullFileNameInDump, fullFileNameInS3, -1)
 	}
 	ioutil.WriteFile(wordpressDumpPath, []byte(wordpressDump), 0644)
 	fmt.Println(wordpressDump[1])
